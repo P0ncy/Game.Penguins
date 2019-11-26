955b5e77a7ca0ad6eddb43e76106b99dc1c236d0
Poncy <c.poncycalixte@epsi.fr>
2019-11-26 10:27:11 +0100

    IA

---
+++ Game.Penguins/Game.Penguins.Core/Code/Interface/IAI.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Game.Penguins.Core.Interfaces.Game.GameBoard;
+using System.Text;
+
+namespace Game.Penguins.Core.Code.Interface
+{
+    public interface IAi
+    {
+        int PlacementPenguinX { get; set; }
+
+        int PlacementPenguinY { get; set; }
+        IBoard MainBoard { get; }
+
+        /*public IAEasy(IBoard plateauParam)
+        {
+            MainBoard = plateauParam;
+            _movementManager = new MovementVerificationHelper(MainBoard);
+        }*/
+    }
+}
---
+++ Game.Penguins/Game.Penguins/IA.easy/IAEasy.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Game.Penguins.Core.Interfaces.Game.GameBoard;
+using Game.Penguins.Core.Interfaces.Game.Actions;
+using Game.Penguins.Core.Code.Interface;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.Penguins.IA.easy
+{
+    class IAEasy : IAi
+    {
+        public int PlacementPenguinX { get; set; }
+
+        public int PlacementPenguinY { get; set; }
+
+        public IBoard MainBoard { get; }
+
+    }
+}
